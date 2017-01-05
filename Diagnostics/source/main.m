5f8e43e66e4a5806babbf96f5833c9f76528d931
Danuta Brotikovskaya <dana.b2012@yandex.ru>
2017-01-05 23:58:09 +0300

    Single image mask creation

--- Diagnostics/source/main.m
+++ Diagnostics/source/main.m
@@ -1,1 +1,14 @@
 % This is main file of algorithm
+
+path = '../data/segmented/';
+filename = 'all1.jpg';
+
+I = imread(strcat(path, filename));
+M = I(:, :, 1)>I(:, :, 2);
+
+se = strel('disk',4);
+se2 = strel('disk',2);
+
+M = imdilate(imerode(M, se), se2);
+
+imshow(M);
