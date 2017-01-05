e7d3e968bbc65184c0376e7ee97a561047085c20
Danuta Brotikovskaya <dana.b2012@yandex.ru>
2017-01-06 02:29:11 +0300

    Main shape orientation calculation through region props

--- Diagnostics/source/main.m
+++ Diagnostics/source/main.m
@@ -1,7 +1,7 @@
 % This is main file of algorithm
 
 path = '../data/segmented/';
-filename = 'all1.jpg';
+filename = 'ryth.jpg';
 
 I = imread(strcat(path, filename));
 M = I(:, :, 1)>I(:, :, 2);
@@ -10,5 +10,26 @@ se = strel('disk',4);
 se2 = strel('disk',2);
 
 M = imdilate(imerode(M, se), se2);
+rp = regionprops(M, 'Centroid', 'MajorAxisLength', 'MinorAxisLength', 'Orientation');
+B = edge(M);
 
-imshow(M);
+if rp.Orientation<0
+    alpha = rp.Orientation + 90 + 30;
+else
+    alpha = rp.Orientation - 30;
+end
+
+cX = rp.Centroid(1);
+cY = rp.Centroid(2);
+minX = cX - (rp.MajorAxisLength/2)*cosd(alpha);
+
+k = tand(180-alpha);
+b = cY - k * cX;
+
+for x=minX:20:cX
+     midY = k * x + b;
+     x, midY
+end
+
+imshow(B);
+line([minX, cX], [k * minX + b, cY]);
